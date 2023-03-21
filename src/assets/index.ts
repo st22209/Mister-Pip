afd105f74c11ceb1453ebcbf0711a2fb503322c1
FusionSid <siddheshadsv@icloud.com>
2023-03-22 00:06:06 +1300

    chore: add image assets of books and author

---
+++ src/assets/index.ts
@@ -0,0 +1,23 @@
+import Book1 from "./book1.jpg"
+import Book2 from "./book2.jpg"
+import Book3 from "./book3.jpg"
+import Book4 from "./book4.jpg"
+import Book5 from "./book5.jpg"
+import Book6 from "./book6.jpg"
+import Author1 from "./author1.jpg";
+import Author2 from "./author2.jpg";
+
+const books = [
+    ["The Cage", Book2],
+    ["The Fish", Book3],
+    ["Hand Me Down World", Book4],
+    ["The Book of Fame", Book5],
+    ["A History of Silence: A Memoir (NZ Ed)", Book6],
+    ["Here at the End of the World We Learn to Dance", Book1],
+]
+
+export {
+    books,
+    Author1,
+    Author2,
+}
